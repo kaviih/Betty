70bffd3e55e6893908ed84b4f75d15497eb2e9b2
Akani Hlongwane <akanihlongwane7@gmail.com>
2023-02-16 13:58:32 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Programme that prints a line to the standard error.
+ * main - Programme that prints a line to the standard error
  * Return: 1 (Success)
  */
 int main(void)
