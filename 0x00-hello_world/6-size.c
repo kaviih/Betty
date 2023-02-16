c5d95c019a4a98fd664933edbd67e0c25a52ee47
Akani Hlongwane <akanihlongwane7@gmail.com>
2023-02-16 12:52:45 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Size of various types on the computer it is compiled and run on.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	char b;
+	long int c;
+	long long int d;
+	float e;
+}
